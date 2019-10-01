555e1b49156163f8b735e7de06693432334586ae
Nicolas Badoux <n.badoux@hotmail.com>
2019-10-01 14:19:24 +0200

    stereo

--- example/run_camera_slam.cc
+++ example/run_camera_slam.cc
@@ -4,10 +4,15 @@
 #include "socket_publisher/publisher.h"
 #endif
 
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+
 #include "openvslam/system.h"
 #include "openvslam/config.h"
 
 #include <iostream>
+#include <thread>
 #include <chrono>
 #include <numeric>
 
@@ -23,12 +28,9 @@
 #include <gperftools/profiler.h>
 #endif
 
-void mono_tracking(const std::shared_ptr<openvslam::config>& cfg,
+void stereo_tracking(const std::shared_ptr<openvslam::config>& cfg,
                    const std::string& vocab_file_path, const unsigned int cam_num, const std::string& mask_img_path,
                    const float scale, const std::string& map_db_path) {
-    // load the mask image
-    const cv::Mat mask = mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE);
-
     // build a SLAM system
     openvslam::system SLAM(cfg, vocab_file_path);
     // startup the SLAM process
@@ -43,16 +45,60 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg,
 #endif
 
     auto video = cv::VideoCapture(cam_num);
+    auto video1 = cv::VideoCapture(cam_num+1);
     if (!video.isOpened()) {
         spdlog::critical("cannot open a camera {}", cam_num);
         SLAM.shutdown();
         return;
     }
+    if (!video1.isOpened()) {
+        spdlog::critical("cannot open a camera {}", cam_num+1);
+        SLAM.shutdown();
+        return;
+    }
+    video.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
+    video.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
 
     cv::Mat frame;
+    cv::Mat frameb;
+    cv::Mat frame2;
+    cv::Mat frame2b;
     double timestamp = 0.0;
     std::vector<double> track_times;
 
+    // variables for stereo image rectification
+    cv::Mat Matrix1 = (cv::Mat_<double>(3, 3) <<257.26448736*1920/640,   0.        , 320.28653362*1920/640,   0.        ,
+                   375.90182075*1080/480, 204.42398179*1080/480,   0.        ,   0.        ,
+                            1        );
+    cv::Mat dist1 = (cv::Mat_<double>(1, 14) <<-0.20370238,  0.05177081,  0.        ,  0.        ,  0.        ,
+                    0.        ,  0.        ,  0.01081185,  0.        ,  0.        ,
+                            0.        ,  0.        ,  0.        ,  0);
+    cv::Mat Rect_1 = (cv::Mat_<double>(3, 3) <<0.99924677, -0.0249036 , -0.02976071,  0.02464204,  0.99965471,
+                   -0.00912362,  0.02997765,  0.00838338,  0.99951541);
+    cv::Mat Proj1 = (cv::Mat_<double>(3, 4) << 375.90182075*1920/640,   0.        , 463.05285645*1920/640,   0.        ,
+                     0.        , 375.90182075*1080/480,  69.40427017*1080/480,   0.        ,
+                              0.        ,   0.        ,   1.        ,   0.);
+
+    cv::Mat Matrix2 = (cv::Mat_<double>(3, 3) << 257.26448736*1920/640,   0.        , 329.35377012*1920/640,   0.        ,
+                   375.90182075*1080/480, 220.41237496*1080/480,   0.        ,   0.        ,
+                            1. );
+    cv::Mat dist2 = (cv::Mat_<double>(1, 14) <<-0.20582674,  0.05086961,  0.        ,  0.        ,  0.        ,
+                    0.        ,  0.        ,  0.01004183,  0.        ,  0.        ,
+                            0.        ,  0.        ,  0.        ,  0.);
+    cv::Mat Proj2 = (cv::Mat_<double>(3, 4) << 375.90182075*1920/640,   0.        , 463.05285645*1920/640, 822.95633477,
+                     0.        , 375.90182075*1080/480,  69.40427017*1080/480,   0.        ,
+                              0.        ,   0.        ,   1.        ,   0.);
+    cv::Mat Rect_2 = (cv::Mat_<double>(3, 3) <<  0.99952138, -0.02565789,  0.01728232,  0.02550557,  0.99963438,
+                    0.00897665, -0.01750633, -0.00853156,  0.99981035);
+    
+
+    const auto cols = cfg->camera_->cols_;
+    const auto rows = cfg->camera_->rows_;
+
+    cv::Mat M1_l, M2_l, M1_r, M2_r;
+    cv::Size imageSize = cv::Size(1920, 720);
+    cv::initUndistortRectifyMap(Matrix1, dist1, Rect_1, Proj1.rowRange(0,3).colRange(0,3), imageSize, CV_32F, M1_l, M2_l);
+    cv::initUndistortRectifyMap(Matrix2, dist2, Rect_2, Proj2.rowRange(0,3).colRange(0,3), imageSize, CV_32F, M1_r, M2_r);
     unsigned int num_frame = 0;
 
     bool is_not_end = true;
@@ -65,17 +111,35 @@ void mono_tracking(const std::shared_ptr<openvslam::config>& cfg,
             }
 
             is_not_end = video.read(frame);
-            if (frame.empty()) {
+            is_not_end = video1.read(frame2) && is_not_end;
+            if (frame.empty() || frame2.empty()) {
                 continue;
             }
+            //*
             if (scale != 1.0) {
                 cv::resize(frame, frame, cv::Size(), scale, scale, cv::INTER_LINEAR);
+                cv::resize(frame2, frame2, cv::Size(), scale, scale, cv::INTER_LINEAR);
+            }//*/
+            cv::remap(frame, frameb, M1_l, M2_l, cv::INTER_LINEAR);
+            cv::remap(frame2, frame2b, M1_r, M2_r, cv::INTER_LINEAR);
+            /*
+            bool result = false;
+            try
+            {
+                result = imwrite("alpha.png", frameb);
             }
-
+            catch (const cv::Exception& ex)
+            {
+                fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
+            }
+            if (result)
+                printf("Saved PNG file with alpha data.\n");
+            else
+                printf("ERROR: Can't save PNG file.\n"); //*/
             const auto tp_1 = std::chrono::steady_clock::now();
 
             // input the current frame and estimate the camera pose
-            SLAM.feed_monocular_frame(frame, timestamp, mask);
+            SLAM.feed_stereo_frame(frame2b, frameb, timestamp);
 
             const auto tp_2 = std::chrono::steady_clock::now();
 
@@ -179,7 +243,9 @@ int main(int argc, char* argv[]) {
 
     // run tracking
     if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Monocular) {
-        mono_tracking(cfg, vocab_file_path->value(), cam_num->value(), mask_img_path->value(),
+    }
+    else if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Stereo) {
+        stereo_tracking(cfg, vocab_file_path->value(), cam_num->value(), mask_img_path->value(),
                       scale->value(), map_db_path->value());
     }
     else {
